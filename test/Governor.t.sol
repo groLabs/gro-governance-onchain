8c681c148fe73e1720ab3ddcf8bf1371e2ca5a85
SHAKOTN <shakotn@protonmail.com>
2023-06-02 16:07:06 +0300

    [tests] - add one more test to check if proposal can be cancelled

--- test/Governor.t.sol
+++ test/Governor.t.sol
@@ -32,12 +32,45 @@ contract GovernorBaseTest is BaseFixture {
         vm.stopPrank();
 
         assertGt(proposalId, 0);
-        // Check that proposal was not created
+        // Check that proposal was created
         assertGt(governor.proposalSnapshot(proposalId), 0);
 
         // Check proposal state
         IGovernor.ProposalState state = governor.state(proposalId);
         assertEq(uint256(state), uint256(IGovernor.ProposalState.Pending)); // Pending
+    }
+
+    function testCreateAndCancelProposal() public {
+        vm.startPrank(based);
+        address[] memory targets = new address[](1);
+        targets[0] = address(0);
+        uint256[] memory values = new uint256[](1);
+        values[0] = 0;
+
+        bytes[] memory calldatas = new bytes[](1);
+        calldatas[0] = abi.encodeWithSignature("test()");
+
+        // Give some voting power to proposer
+        aggregator.setBalance(based, governor.PROPOSAL_THRESHOLD());
+
+        uint256 proposalId = governor.propose(
+            targets,
+            values,
+            calldatas,
+            "test"
+        );
+        vm.stopPrank();
+        // Check proposal state
+        IGovernor.ProposalState state = governor.state(proposalId);
+        assertEq(uint256(state), uint256(IGovernor.ProposalState.Pending));
+
+        // Try cancel now
+        vm.startPrank(based);
+        governor.cancel(targets, values, calldatas, keccak256(bytes("test")));
+
+        // Check proposal again and make sure it was canceled
+        state = governor.state(proposalId);
+        assertEq(uint256(state), uint256(IGovernor.ProposalState.Canceled));
     }
 
     /// @notice Testing revert when proposer creating proposal without enough voting power
